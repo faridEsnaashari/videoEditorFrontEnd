0df5b407ae64fa42254404445d85593c9810a837
faridEsnaashari <faride13578642@gmail.com>
2020-12-11 21:04:41 +0330

    first structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
 import { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/styles';
 
 import testVideo from './videos/2.mp4';
 
+const styles = {
+    container: {
+        width: "90vw",
+        margin: "auto",
+        display: "flex",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+    },
+    video: {
+        width: "100%",
+    },
+    timeLineContainer: {
+        height: "20%",
+    },
+    timeLineTapeContainer: {
+        position: "relative",
+        height: "50%",
+    },
+    timeLineButtonContainer: {
+        height: "50%",
+    },
+    videoAndToolsContainer: {
+        width:"49%",
+    },
+
+    timeLineTap: {
+        position: "absolute",
+        background: "none",
+        pointerEvents: "none",
+        width: "100%",
+        height: "100%",
+
+        "&::-moz-range-track":{
+            background: "none",
+        },
+        "&::-moz-range-thumb":{
+            pointerEvents: "auto",
+            borderRadius: "unset",
+            width: "10%",
+            height: "100%",
+            clipPath: "polygon(0% 0%, 40% 10%, 40% 100%, 60% 100%, 60% 10%, 100% 0%)",
+        },
+    },
+
+    "@media (max-width: 950px)": {
+        videoAndToolsContainer: {
+            width:"100%",
+        }
+    },
+};
 function App(props) {
+    const classes = makeStyles(styles)();
+    console.log(classes);
     const [startTime, setStartTime] = useState(0);
     const [endTime, setEndTime] = useState(0);
     const [state, setState] = useState("startTime");
@@ -41,16 +94,31 @@ function App(props) {
     };
 
     return (
-        <>
-            <video id="originalVideo" width="500px" height="500px" autoPlay controls>
-                <source src={ testVideo }/>
-            </video>
-            <div>
-                <input id="startTime" type="range" min="0" max={ originalVideoEndTime } step="0.1" onChange={ startTimeChanged }/>
-                <input id="endTime" type="range" min="0" max={ originalVideoEndTime } step="0.1" onChange={ endTimeChanged }/>
-                <button onClick={ submit }>submit</button>
+        <div className={ classes.container }>
+            <div className={ classes.videoAndToolsContainer }>
+                <video className={ classes.video } id="originalVideo" autoPlay controls>
+                    <source src={ testVideo }/>
+                </video>
+                <div className={ classes.timeLineContainer }>
+                    <div className={ classes.timeLineButtonContainer }>
+                        <button onClick={ submit }>previousAction</button>
+                        <button onClick={ submit }>submit</button>
+                    </div>
+                    <div className={ classes.timeLineTapeContainer }>
+                        <input className={ classes.timeLineTap } id="startTime" type="range" min="0" max={ originalVideoEndTime } step="0.1" onChange={ startTimeChanged }/>
+                        <input className={ classes.timeLineTap } id="endTime" type="range" min="0" max={ originalVideoEndTime } step="0.1" onChange={ endTimeChanged }/>
+                    </div>
+                </div>
             </div>
-        </>
+            <div className={ classes.videoAndToolsContainer }>
+                <video className={ classes.video } id="resultVideo" autoPlay>
+                    <source src={ testVideo }/>
+                </video>
+                <div className={ classes.timeLineContainer }>
+                    <button onClick={ submit }>play/pouse</button>
+                </div>
+            </div>
+        </div>
     );
 }
 
