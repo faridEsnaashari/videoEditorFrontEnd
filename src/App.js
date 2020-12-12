c5c5d0ee47199dd0431c7a43c565c606b7421b5f
faridEsnaashari <faride13578642@gmail.com>
2020-12-12 18:37:38 +0330

    add a testvideo to VideoEditor component as its props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import VideoEditor from './components/VideoEditor';
+import testVideo from './videos/2.mp4';
 
 function App(){
     const sendTimes = times => console.log(times);
     return (
-        <VideoEditor onTimesReady={ sendTimes }/>
+        <VideoEditor onTimesReady={ sendTimes } video={ testVideo }/>
     );
 }
 
